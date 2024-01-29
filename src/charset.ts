657117f30269713e2f2f9070ee1a009536685b3e
Patrick Pace <pqp@coapt.net>
2024-01-29 18:02:03 -0500

    Alphabet characters are drawn

--- src/charset.ts
+++ src/charset.ts
@@ -54,9 +54,161 @@ xx
 'g':
 `
 xxxxxxxx
+xx      
+xx  xxxx
+xx    xx
 xxxxxxxx
+`,
+'h':
+`
+xx    xx
+xx    xx
 xxxxxxxx
+xx    xx
+xx    xx
+`,
+'i':
+`
 xxxxxxxx
+   xx   
+   xx   
+   xx   
+xxxxxxxx
+`,
+'j':
+`
+xxxxxxxx
+     xx 
+     xx 
+xx   xx 
+xxxxxxx 
+`,
+'k':
+`
+xx    xx
+xx   xx 
+xxxxxx  
+xx   xx 
+xx    xx
+`,
+'l':
+`
+xx      
+xx      
+xx      
+xx      
+xxxxxxxx
+`,
+'m':
+`
+xx    xx
+xxx  xxx
+x  xx  x
+x  xx  x
+x  xx  x
+`,
+'n':
+`
+xx    xx
+xxx   xx
+xx x  xx
+xx  x xx
+xx   xxx
+`,
+'o':
+`
+xxxxxxxx
+xx    xx
+xx    xx
+xx    xx
+xxxxxxxx
+`,
+'p':
+`
+xxxxxxxx
+x     xx
+x     xx
+xxxxxxxx
+xx      
+`,
+'q':
+`
+xxxxxxxx
+xx    xx
+xxxxxxxx
+   xxx  
+     xxx
+`,
+'r':
+`
+xxxxxxxx
+x     xx
+xxxxxxxx
+xx  xx  
+xx  xxxx
+`,
+'s':
+`
+xxxxxxxx
+xx      
+xxxxxxxx
+      xx
+xxxxxxxx
+`,
+'t':
+`
+xxxxxxxx
+   xx   
+   xx   
+   xx   
+   xx   
+`,
+'u':
+`
+xx    xx
+xx    xx
+xx    xx
+xx    xx
+xxxxxxxx
+`,
+'v':
+`
+x      x
+xx    xx
+ xx  xx 
+  xxxx  
+   xx   
+`,
+'w':
+`
+x  xx  x
+x  xx  x
+x  xx  x
+x  xx  x
+xxxxxxxx
+`,
+'x':
+`
+xx    xx
+  x  x  
+   xx   
+  x  x  
+xx    xx
+`,
+'y':
+`
+xx    xx
+ xx  xx 
+  xxxx  
+   xx   
+   xx   
+`,
+'z':
+`
+xxxxxxxx
+     xx 
+    xx  
+  xx    
 xxxxxxxx
 `,
 'block':
